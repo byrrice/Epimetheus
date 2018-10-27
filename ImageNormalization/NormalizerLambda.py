59532141cb5503bac46e0870a521caaa8568a5d7
amt139 <amt139@case.edu>
2018-10-27 12:32:05 -0400

    got the functions for 'imageToArray' and 'imageDiff' working

---
+++ ImageNormalization/NormalizerLambda.py
@@ -0,0 +1,15 @@
+from scipy import misc
+import numpy as np
+
+
+def imageToArray(filename):
+    mapScreenShot = misc.imread(filename)
+    mapScreenShotArray = np.array(mapScreenShot)
+
+    return mapScreenShotArray
+
+
+def imageDiff(cameraImageArray, apiImageArray):
+    diffArray = np.subtract(cameraImageArray, apiImageArray)
+    return diffArray
+

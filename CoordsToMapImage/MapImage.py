b1b01da049657a16bb2847f9dd12546912d8ba53
Rahul <pepsi1423@gmail.com>
2018-10-27 18:03:14 -0400

    Added test images and changed method for saving images and test code

--- CoordsToMapImage/MapImage.py
+++ CoordsToMapImage/MapImage.py
@@ -2,10 +2,8 @@ from PIL import Image
 from io import BytesIO
 import requests
 
-
 # Coordinates of the center of the image, and zoom based upon FOV and altitude of image
 from APIKey import ReturnAPIKey
-
 
 def getSatImage(coordLat, coordLong, zoom=18):
     URL = "https://maps.googleapis.com/maps/api/staticmap?"
@@ -13,16 +11,13 @@ def getSatImage(coordLat, coordLong, zoom=18):
     # Crop out top and bottom 20 pixels to keep image centered
     size = '1080x1120'
     maptype = 'satellite'
-
+    key = ''
     PARAMS = {'center':center,'zoom':zoom,'size':size,'maptype':maptype,'key':key}
     
     r = requests.get(url=URL, params=PARAMS)
     img = Image.open(BytesIO(r.content))
     width, height = img.size
     img = img.crop((0,20,width,(height-20)))
-    # img.show()
-    return img
-
-if __name__ == "__main__":
-    img = getSatImage(40.714728,-73.998672)
-    img.show()
+    fileName = 'Images\img1.png'
+    img.save(fileName, 'PNG')
+    return fileName
---
+++ tester.py
@@ -0,0 +1,5 @@
+from Main.mainRunner import run
+
+if __name__ == "__main__":
+    run(40.0065128,-83.03053321237,'Images\img.png')
+    # img.show()

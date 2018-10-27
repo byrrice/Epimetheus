f4e729b9fb75821a6f92aa1248d18d2de5c0e61e
amt139 <amt139@case.edu>
2018-10-27 13:54:30 -0400

    got convert world coord to lat and long working

--- ImageNormalization/CoordinateConverter.py
+++ ImageNormalization/CoordinateConverter.py
@@ -1,6 +1,22 @@
 import numpy as np
 
 
-def converCoords(pixelCoord, zoomLevel):
-    worldCoord = np.multiply(pixelCoord, np.power(2, -zoomLevel))
-    return worldCoord
+def convertPixelToWorld(pixelCoordX, pixelCoordY, zoomLevel):
+    worldCoordX = np.multiply(pixelCoordX, np.power(2, -zoomLevel))
+    worldCoordY = np.multiply(pixelCoordY, np.power(2, -zoomLevel))
+
+    return [worldCoordX, worldCoordY]
+
+
+def convertWorldToLongLat(worldCoordArray):
+    longitude = np.subtract(
+            np.multiply((np.divide(worldCoordArray[0], 256)), 360), 180)
+
+    insideFunc = np.subtract(np.pi, (
+        np.divide(np.multiply(np.multiply(2, np.pi), worldCoordArray[1]), 256)))
+
+    latitude = np.multiply(np.divide(180, np.pi)), np.atan(
+            np.multiply(0.5,
+                        np.subtract(np.exp(insideFunc), np.exp(-insideFunc))))
+
+    return [latitude, longitude]

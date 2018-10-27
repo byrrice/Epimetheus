abb17d1a9a370a112806c1752cf73a39083a6a04
amt139 <amt139@case.edu>
2018-10-27 15:55:31 -0400

    gets road list from obstruction latLong array

--- ImageNormalization/GetAPIData.py
+++ ImageNormalization/GetAPIData.py
@@ -1,16 +1,28 @@
 import requests
 from APIKey import ReturnAPIKey as key
 from ImageNormalization import CoordinateConverter as convert
-from ImageNormalization import GetLatLongFromPix as getLatLong
+from ImageNormalization import Normalizer as obstructionArray
+from ImageNormalization import GetLatLongFromPix as latLong
+
 
 def obstructionToLongLat():
+    diffArray = obstructionArray.imageDiff()
+    convertedArray = []
+
+    for i in range(0, 1080):
+        for j in range(0, 1080):
+            if diffArray[i][j] > 0:
+                convertedArray.append(
+                        tuple((latLong.getLatFromPix(i, latitudeBottom,
+                                                     latitudeTop),
+                               latLong.getLongFromPix(j, longitudeLeft,
+                                                      longitudeRight))))
+
+    return convertedArray
 
 
-
-
-def getRoadAPIData(latitude, longitude):
-
-    parameters = "points=" + latitude + "," + longitude
+def getRoadAPIData(latLong):
+    parameters = "points=" + latLong[0] + "," + latLong[1]
     apikey = "&key=" + key.throwAPIKey()
     url = "https://roads.googleapis.com/v1/nearestRoads?" + parameters + apikey
 
@@ -20,8 +32,8 @@ def getRoadAPIData(latitude, longitude):
     placeID = data["placeId"]
     return placeID
 
-def getPlaceAPIData(placeIDArray):
 
+def getPlaceAPIData(placeIDArray):
     apikey = "&key=" + key.throwAPIKey()
     parameters = "json?placeID=" + placeIDArray
     url = "https://maps.googleapis.com/maps/api/place/details/"
@@ -32,3 +44,11 @@ def getPlaceAPIData(placeIDArray):
     roadName = data["result"]["formatted_address"]
     return roadName
 
+
+def getRoadList(convertedArray):
+    roadList = []
+
+    for i in range(0, convertedArray.length):
+        roadList.append(getPlaceAPIData(getRoadAPIData(convertedArray[i])))
+
+    return roadList

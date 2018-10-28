febca3c2357b5b30e50e55c469857ce1dbc40e43
Rahul <pepsi1423@gmail.com>
2018-10-28 08:20:18 -0400

    More test images

--- mainRunner.py
+++ mainRunner.py
@@ -11,4 +11,13 @@ def run(centerLat, centerLong, droneImgPath):
     return getRoads.doEverything(droneImgPath, satImgPath, roadImgPath, botlat, toplat, leftlong, rightlong)
 
 if __name__ == "__main__":
-    print(run(40.0065128,-83.03053321237,'./Images/img.png'))
+    # print(run(40.0065128,-83.03053321237,'./Images/testImage1.png'))
+    # print("Done 1")
+    # input()
+    # print(run(39.9959124,-82.9981831,'./Images/testImage2.png'))
+    # print("Done 2")
+    # input()
+    print(run(39.8582479,-83.2199184,'./Images/testImage3.png'))
+    # print("Done 3")
+    # input()
+    # print(run(39.8512809,-83.1647455,'./Images/testImage4.png'))
